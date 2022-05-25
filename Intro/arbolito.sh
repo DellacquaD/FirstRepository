f3180c18258653b164ed9c216eac74a186e33055
ddellacqua <ddellacqua@fcien.edu.uy>
2022-05-25 12:29:03 -0300

    agrego carpetas node

---
+++ Intro/arbolito.sh
@@ -0,0 +1,32 @@
+#! / bin / sh.
+mkdir Peliculas
+mkdir Peliculas/Infantiles
+mkdir Peliculas/Accion
+mkdir Peliculas/Terror
+mkdir Peliculas/Comedia
+mkdir Peliculas/Romanticas
+
+touch Peliculas/Infantiles/Intenzamente.txt
+touch Peliculas/Infantiles/"El juego del miedo.xls"
+touch Peliculas/Infantiles/"Rapido y Furioso.xls"
+touch Peliculas/Infantiles/titanic.jpg
+
+touch Peliculas/Accion/"Smooth criminal.txt"
+touch Peliculas/Accion/"Smooth criminal.txt"
+touch Peliculas/Accion/"Smooth criminal.txt"
+touch Peliculas/Accion/Cars.jpg
+
+touch Peliculas/Terror/"La dama y el bagavundo.png"
+touch Peliculas/Terror/"Yo antes de ti.xls"
+touch Peliculas/Terror/"Que paso ayer.pdf" 
+touch Peliculas/Terror/"We will rock you.js"
+
+touch Peliculas/Comedia/Madagaskar.js
+touch Peliculas/Comedia/"Orgullo y prejuicio.txt" 
+touch Peliculas/Comedia/IT.xls
+touch Peliculas/Comedia/"Mision imposible.png"
+
+touch Peliculas/Romanticas/Pocahontas.jpg
+touch Peliculas/Romanticas/Annabelle.js
+touch Peliculas/Romanticas/"Los vengadores.js"
+touch Peliculas/Romanticas/"La mascara.js"
---
+++ ProgramacionImperactiva/app.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// let nombre = prompt("ingrese su nombre: ");
+// console.log(`Hola ${nombre}!!!`);
+
+let altura = 100;
+let ciudad = "Montevideo";
+let mayorDeEdad = true;
+
+console.log(altura)
+console.log(ciudad)
+console.log(mayorDeEdad)
+
+console.log(altura, ciudad, mayorDeEdad)
+console.log(`mi altura es: ` )
